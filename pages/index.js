20f4998acda3e1c04142dde7bfe4e7b7aa28fefe
rickgrael <ricardochvs23@gmail.com>
2020-06-04 13:53:08 -0300

    ultima tentativa

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">ola porra.js!</a>
         </h1>
+        <h2 className='title'> agora deu certo</h2>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
